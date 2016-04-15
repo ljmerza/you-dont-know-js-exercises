23cbb762cdafeb253b78433f77c0e8ad7410a29b
ljmerza <ljmerza@gmail.com>
2016-04-15 00:04:42 -0400

    update bk2

--- 02.scope and closures/chpt2.js
+++ 02.scope and closures/chpt2.js
@@ -1,1 +1,49 @@
-/*************lexical scope******************/
+/*************lexical scope*****************
+-two types of scopes, dynamic and lexical
+-first stage of compiing is lexing or tokenizing
+-scope is defined while tokenizing or lexing - 
+-depends on where vars and blocks of a scope are created*/
+function foo(a) {
+	var b = a * 2
+	function bar(c){
+		console.log(a,b,c)
+	}
+	bar(b * 3)
+}
+foo(2)
+/*three scopes, global, inside foo, and inside bar functions
+-same identifier name can be used in different scopes (first one found
+	up scope hierarchy is used) is called "shadowing"
+-lexical scope only defined by where function is declared
+	-lookup process only applies to first class identifiers (such as a,b,c)
+	-if calling foo.bar.c, causes look up of foo object then property access after that
+
+/*****cheating lexical*******/
+/*cheating lexical scope leads to bad performance - 2 ways to cheat
+-eval() parameter is string that is executed as js
+	can be used to modify code that engine executes to change lexical scope*/
+function foo(str, a){
+	eval(str)
+	console.log(a, b)
+}
+var b = 2
+foo("var b = 3;", 1)
+/*eval code creates new var b in foo's scope so global var b isnt looked up anymore
+	-use strict mode causes eval() to be in its own lexical scope
+	-setInterval() and setTimeout() can be passed a string to eval
+	-new Function() constructor takes string as last arg to ceate a function dynamically
+-'with' keyword can be used to change scope*/
+function foo(obj){
+	with (obj){
+		a = 2
+	}
+}
+var o1 = { a: 3 }
+console.log(o1.b)
+/*with block is treated as a seperate scope
+-o1.b doesnt exist so when obj called in function is undefined, when var a
+	is assigned in function, since obj is undefined, a global var a is created instead
+-since eval() and 'with' change lexical scope the engine cant optimize since scopes are changing*/
+
+
+
---
+++ 02.scope and closures/chpt3.js
@@ -0,0 +1,42 @@
+/***************function vs block scopes************/
+/****scopes from functions*****/
+function foo(a){
+	var b = 2
+	function bar(){
+
+	}
+	var c = 3
+}
+/*vars a,b,c belong in foo's scope - where they are in scope doesnt matter
+-accessing vars outside scope cause ReferenceErrors
+-follow principle of least privilege - expose vars that are only needed for operation
+	-avoids name collisions - vars with same name mixing - can happen in global scope
+-create single global object and use it as namespace of objects*/
+var myLib = {
+	a: = 2,
+	funct: function(){},
+	funct2: function ()
+} // access by myLib.a, myLib.funct
+/*modern collision avoidance is module creation using dependency managers*/
+
+/*****functions as scopes*****/
+/*can wrap section of in a function to hide it in a scope*/
+var a = 2
+function(){
+	var a = 3
+	console.log(a) // outputs 3
+}
+foo() // outputs 3
+console.log(a) // outputs 2
+/*proplems with this method:
+	-declared function foo pollutes enclosing (global here) scope
+	-use IIFE - creates function expression*/
+var a = 2
+(function foo(){
+	var a = 3
+	console.log(a)
+})()
+console.log(a)
+/*
+
+
